207279b0097e96471933bd777beb1c143d2bed93
GuntramBechtold <guntram.bechtold@starsmedia.com>
2025-06-28 20:50:55 +0200

    Fix deployment configuration for i18n routing

--- Dockerfile
+++ Dockerfile
@@ -24,6 +24,7 @@ ENV NODE_ENV production
 COPY --from=builder /app/public ./public
 COPY --from=builder /app/.next/standalone ./
 COPY --from=builder /app/.next/static ./.next/static
+COPY --from=builder /app/next-i18next.config.js ./
 
 EXPOSE 3000
 
--- next.config.js
+++ next.config.js
@@ -6,6 +6,15 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'standalone',
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/en',
+        permanent: false,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
